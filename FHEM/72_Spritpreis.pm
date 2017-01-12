a739c30fd9005073da5af628ce08e7700adc8136
Peter Jakobs <pjakobs@mobileme.de>
2017-01-12 20:54:01 +0100

    working on finding stations for a textual address

--- FHEM/72_Spritpreis.pm
+++ FHEM/72_Spritpreis.pm
@@ -49,8 +49,8 @@ Spritpreis_Define($$) {
     my ($hash, $def)=@_;
     my @parts=split("[ \t][ \t]*", $def);
     my $name=$parts[0];
-    Spritpreis_Tankerkoenig_GetPricesForLocation($hash);
-    InternalTimer(gettimeofday()+AttrVal($hash->{NAME}, "interval",15)*60, "Spritpreis_Tankerkoenig_GetPricesForLocation",$hash);
+    # Spritpreis_Tankerkoenig_GetPricesForLocation($hash);
+    # InternalTimer(gettimeofday()+AttrVal($hash->{NAME}, "interval",15)*60, "Spritpreis_Tankerkoenig_GetPricesForLocation",$hash);
     return undef;
 }
 
@@ -67,8 +67,28 @@ Spritpreis_Set(@) {
 sub
 Spritpreis_Get(@) {
     my ($hash, $name, $cmd, @args) = @_;
-    Spritpreis_Tankerkoenig_GetPricesForLocation($hash);
-    Spritpreis_GetCoordinatesForAddress($hash,"Hamburg, Elbphilharmonie");
+
+    return "Unknown command $cmd, choose one of search" if ($cmd eq '?');
+    Log3($hash, 3,"$hash->{NAME}: get $hash->{NAME} $cmd $args[0]");
+
+    if ($cmd eq "search"){
+        my $str='';
+        my $i=0;
+        while($args[$i++]){
+            $str=$str." ".$args[$i];
+        }
+        Log3($hash,4,"$hash->{NAME}: search string: $str");
+        my @loc=Spritpreis_GetCoordinatesForAddress($hash, $str);
+        my ($lat, $lng, $str)=@loc;
+        if($lat==0 && $lng==0){
+            return $str;
+        }else{
+            my $ret=Spritpreis_GetStationIDsForLocation($hash, @loc);
+            return $ret;
+        }
+    }
+    #Spritpreis_Tankerkoenig_GetPricesForLocation($hash);
+    #Spritpreis_GetCoordinatesForAddress($hash,"Hamburg, Elbphilharmonie");
     # add price trigger here
     return undef;
 }
@@ -132,15 +152,19 @@ Spritpreis_Tankerkoenig_GetPricesForIDs(@){
 }
 
 sub
-Spritpreis_Tankerkoenig_GetPricesForLocation(@){
-   my ($hash) = @_;
+Spritpreis_GetStationIDsForLocation(@){
+   my ($hash,@location) = @_;
 
-   my $lat=AttrVal($hash->{'NAME'}, "lat",0);
-   my $lng=AttrVal($hash->{'NAME'}, "lon",0);
+   # my $lat=AttrVal($hash->{'NAME'}, "lat",0);
+   # my $lng=AttrVal($hash->{'NAME'}, "lon",0);
    my $rad=AttrVal($hash->{'NAME'}, "rad",5);
    my $type=AttrVal($hash->{'NAME'}, "type","diesel");
    my $sort=AttrVal($hash->{'NAME'}, "sortby","price");
    my $apikey=AttrVal($hash->{'NAME'}, "apikey","");
+
+   my ($lat, $lng, $formattedAddress)=@location;
+
+   my $result;
 
    if($apikey eq "") {
        Log3($hash,3,"$hash->{'NAME'}: please provide a valid apikey, you can get it from https://creativecommons.tankerkoenig.de/#register. This function can't work without it"); 
@@ -153,14 +177,41 @@ Spritpreis_Tankerkoenig_GetPricesForLocation(@){
    my $param= {
        url      => $url,
        hash     => $hash,
-       timeout  => 30,
+       timeout  => 1,
        method   => "GET",
        header   => "User-Agent: fhem\r\nAccept: application/json",
-       parser   => \&Spritpreis_ParsePricesForLocation,
-       callback => \&Spritpreis_callback
     };
-    HttpUtils_NonblockingGet($param);
-    InternalTimer(gettimeofday()+AttrVal($hash->{NAME}, "interval",15)*60, "Spritpreis_Tankerkoenig_GetPricesForLocation",$hash);
+    my ($err, $data) = HttpUtils_BlockingGet($param);
+
+    if($err){
+        Log3($hash, 4, "$hash->{NAME}: error fetching nformation");
+    } elsif($data){
+        Log3($hash, 4, "$hash->{NAME}: got data");
+        Log3($hash, 5, "$hash->{NAME}: got data $data\n\n\n");
+
+        eval {
+            $result = JSON->new->utf8(1)->decode($data);
+        };
+        if ($@) {
+            Log3 ($hash, 4, "$hash->{NAME}: error decoding response $@");
+        } else {
+            my ($stations) = $result->{stations};
+            my $ret="<html><p><h3>Stations for Address</h3></p><p><h2>$formattedAddress</h2></p><form action='fhem/cmd?set ".$hash->{NAME}." station' method='get'><select multiple name='id'>";
+            foreach (@{$stations}){
+                (my $station)=$_;
+
+                Log3($hash, 2, "Name: $station->{name}, id: $station->{id}");
+                $ret=$ret."<option value=".$station->{id}.">".$station->{name}." ".$station->{place}." ".$station->{street}." ".$station->{houseNumber}."</option>";
+            }
+            $ret=$ret."<button type='submit'>submit</button></html>";
+            Log3($hash,2,"$hash->{NAME}: ############# ret: $ret");
+            return $ret;
+        }         
+    }else {
+        Log3 ($hash, 4, "$hash->{NAME}: something's very odd");
+    }
+    return $data; 
+ InternalTimer(gettimeofday()+AttrVal($hash->{NAME}, "interval",15)*60, "Spritpreis_Tankerkoenig_GetPricesForLocation",$hash);
     return undef;
 }
 
@@ -182,8 +233,6 @@ Spritpreis_callback(@) {
      #Log3($hash, 4, "$hash->{NAME}: calling parser $parser with err $err and data $data");
      &$parser($hash, $err, $data);
  
-     # Do readings update
- 
      if( $err || $err ne ""){
          Log3 ($hash, 3, "$hash->{NAME} Readings NOT updated, received Error: ".$err);
      }
@@ -196,7 +245,7 @@ Spritpreis_ParseIDsForLocation(@){
 }
 
 sub
-Spritpreis_ParsePricesForLocation(@){
+Spritpreis_ParseStationIDsForLocation(@){
     my ($hash, $err, $data)=@_;
     my $result;
 
@@ -216,31 +265,35 @@ Spritpreis_ParsePricesForLocation(@){
         } else {
             my ($stations) = $result->{stations};
             #Log3($hash, 5, "$hash->{NAME}: stations:".Dumper($stations));
-            readingsBeginUpdate($hash);
+            # readingsBeginUpdate($hash);
+            my $ret="<html><form action=fhem/cmd?set ".$hash->{NAME}." station method='get'><select multiple name='id'>";
             foreach (@{$stations}){
                 (my $station)=$_;
 
                 #Log3($hash, 5, "$hash->{NAME}: Station hash:".Dumper($station));
-                Log3($hash, 2, "Name: $station->{name}, id: $station->{id}\n");
-                my $number=0;
-                
+                Log3($hash, 2, "Name: $station->{name}, id: $station->{id}");
+                # my $number=0;
+                $ret=$ret."<option value=".$station->{id}.">".$station->{name}." ".$station->{place}." ".$station->{street}." ".$station->{houseNumber}."</option>";
                 # make sure we update a record with an existign id or create a new one for a new id
-                while(ReadingsVal($hash->{NAME},$number."_id",$station->{id}) ne $station->{id}) 
-                {
-                    $number++;
-                }
-                readingsBulkUpdate($hash,$number."_name",$station->{name});
-                readingsBulkUpdate($hash,$number."_price",$station->{price});
-                readingsBulkUpdate($hash,$number."_place",$station->{place});
-                readingsBulkUpdate($hash,$number."_street",$station->{street}." ".$station->{houseNumber});
-                readingsBulkUpdate($hash,$number."_distance",$station->{dist});
-                readingsBulkUpdate($hash,$number."_brand",$station->{brand});
-                readingsBulkUpdate($hash,$number."_lat",$station->{lat});
-                readingsBulkUpdate($hash,$number."_lon",$station->{lng});
-                readingsBulkUpdate($hash,$number."_id",$station->{id});
-                readingsBulkUpdate($hash,$number."_isOpen",$station->{isOpen});
+                # while(ReadingsVal($hash->{NAME},$number."_id",$station->{id}) ne $station->{id}) 
+                # {
+                #     $number++;
+                # }
+                # readingsBulkUpdate($hash,$number."_name",$station->{name});
+                # readingsBulkUpdate($hash,$number."_price",$station->{price});
+                # readingsBulkUpdate($hash,$number."_place",$station->{place});
+                # readingsBulkUpdate($hash,$number."_street",$station->{street}." ".$station->{houseNumber});
+                # readingsBulkUpdate($hash,$number."_distance",$station->{dist});
+                # readingsBulkUpdate($hash,$number."_brand",$station->{brand});
+                # readingsBulkUpdate($hash,$number."_lat",$station->{lat});
+                # readingsBulkUpdate($hash,$number."_lon",$station->{lng});
+                # readingsBulkUpdate($hash,$number."_id",$station->{id});
+                # readingsBulkUpdate($hash,$number."_isOpen",$station->{isOpen});
             }
-            readingsEndUpdate($hash,1);
+            # readingsEndUpdate($hash,1);
+            $ret=$ret."<button type='submit'>submit</button></html>";
+            Log3($hash,2,"$hash->{NAME}: ############# ret: $ret");
+            return $ret;
         }         
     }else {
         Log3 ($hash, 4, "$hash->{NAME}: something's very odd");
@@ -261,21 +314,47 @@ sub
 Spritpreis_GetCoordinatesForAddress(@){
     my ($hash, $address)=@_;
     
+    my $result;
+
     my $url=new URI::URL 'https://maps.google.com/maps/api/geocode/json';
     $url->query_form("address",$address);
     Log3($hash, 3, "$hash->{NAME}: request URL: ".$url);
 
     my $param= {
     url      => $url,
-    hash     => $hash,
-    timeout  => 30,
+    timeout  => 1,
     method   => "GET",
     header   => "User-Agent: fhem\r\nAccept: application/json",
-    parser   => \&Spritpreis_ParseCoordinatesForAddress,
-    callback => \&Spritpreis_callback
     };
-    HttpUtils_NonblockingGet($param);
-    return undef;
+    my ($err, $data)=HttpUtils_BlockingGet($param);
+
+    if($err){
+        Log3($hash, 4, "$hash->{NAME}: error fetching nformation");
+    } elsif($data){
+        Log3($hash, 4, "$hash->{NAME}: got CoordinatesForAddress reply");
+        Log3($hash, 5, "$hash->{NAME}: got data $data\n\n\n");
+
+        eval {
+            $result = JSON->new->utf8(1)->decode($data);
+        };
+        if ($@) {
+            Log3 ($hash, 4, "$hash->{NAME}: error decoding response $@");
+        } else {
+            if ($result->{status} eq "ZERO_RESULTS"){
+                return(0,0,"error: could not find address");
+            }else{
+                my $lat=$result->{results}->[0]->{geometry}->{location}->{lat};
+                my $lon=$result->{results}->[0]->{geometry}->{location}->{lng};
+                my $formattedAddress=$result->{results}->[0]->{formatted_address};
+
+                Log3($hash,3,"$hash->{NAME}: got coordinates for address as lat: $lat, lon: $lon");
+                return ($lat, $lon, $formattedAddress);
+            }
+        }         
+    }else {
+        Log3 ($hash, 4, "$hash->{NAME}: something is very odd");
+    }
+    return undef; 
 }
 
 sub
@@ -301,14 +380,12 @@ Spritpreis_ParseCoordinatesForAddress(@){
             my $lon=$result->{results}->[0]->{geometry}->{location}->{lng};
 
             Log3($hash,3,"$hash->{NAME}: got coordinates for address as lat: $lat, lon: $lon");
-            # readingsBeginUpdate($hash);
-            
-            # readingsEndUpdate($hash,1);
+            return ($lat, $lon);
         }         
     }else {
         Log3 ($hash, 4, "$hash->{NAME}: something is very odd");
     }
-    return $data; 
+    return undef; 
 }
        
 #####################################
